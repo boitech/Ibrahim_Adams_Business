5b6f77088ac8105a075cf362a83aa73ad9203634
Ibrahim Adams <mribrahimadams1@gmail.com>
2025-02-18 11:21:09 +0300

    .

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
 function redirectToWhatsApp() {
   window.open("https://wa.me/yourphonenumber", "_blank");
 }
+
+function toggleMenu() {
+  const navLinks = document.querySelector(".nav-links");
+  navLinks.classList.toggle("show");
+}
